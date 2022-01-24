b1565adaf247c89fd8252916bb80e7a24b8f694b
OBJECT OF OBSESSION <1752832598@qq.com>
2022-01-24 20:51:52 +0800

    Remove updatePhase()

--- src/main/java/heckerpowered/ultimatetech/common/capabilities/phase/IPhase.java
+++ src/main/java/heckerpowered/ultimatetech/common/capabilities/phase/IPhase.java
@@ -1,20 +1,9 @@
 package heckerpowered.ultimatetech.common.capabilities.phase;
 
-import heckerpowered.ultimatetech.common.network.UltimateTechPacketHandler;
-import heckerpowered.ultimatetech.common.network.UpdatePhasePacket;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-
 public interface IPhase {
     public void setPhase(boolean phase);
 
     public void clearPhase();
 
     public boolean isPhase();
-
-    default void updatePhase(Player player) {
-        if (player instanceof ServerPlayer) {
-            UltimateTechPacketHandler.sendTo(new UpdatePhasePacket(isPhase()), (ServerPlayer) player);
-        }
-    }
 }
