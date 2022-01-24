a674847103309a13c7fdeca40b2de2d469b0a28a
OBJECT OF OBSESSION <1752832598@qq.com>
2022-01-24 20:02:32 +0800

    No Save.

--- src/main/java/heckerpowered/ultimatetech/common/registries/UltimateTechEntity.java
+++ src/main/java/heckerpowered/ultimatetech/common/registries/UltimateTechEntity.java
@@ -9,15 +9,17 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public final class UltimateTechEntity {
-    private UltimateTechEntity() {
-    }
+        private UltimateTechEntity() {
+        }
 
-    public static final DeferredRegister<EntityType<?>> DEFERRED_REGISTER = DeferredRegister.create(
-            ForgeRegistries.ENTITIES,
-            UltimateTech.MODID);
+        public static final DeferredRegister<EntityType<?>> DEFERRED_REGISTER = DeferredRegister.create(
+                        ForgeRegistries.ENTITIES,
+                        UltimateTech.MODID);
 
-    public static final RegistryObject<EntityType<SuperArrow>> SUPER_ARROW = DEFERRED_REGISTER
-            .register("super_arrow",
-                    () -> EntityType.Builder.<SuperArrow>of(SuperArrow::new, MobCategory.MISC).sized(0.5F, 0.5F)
-                            .clientTrackingRange(4).updateInterval(20).build("super_arrow"));
+        public static final RegistryObject<EntityType<SuperArrow>> SUPER_ARROW = DEFERRED_REGISTER
+                        .register("super_arrow",
+                                        () -> EntityType.Builder.<SuperArrow>of(SuperArrow::new, MobCategory.MISC)
+                                                        .sized(0.5F, 0.5F)
+                                                        .clientTrackingRange(4).updateInterval(20).noSave()
+                                                        .build("super_arrow"));
 }
