27db22791c3789d18755dc08e9d6700f96eabb3e
OBJECT OF OBSESSION <1752832598@qq.com>
2022-01-24 20:04:37 +0800

    Copper

---
+++ src/main/java/heckerpowered/ultimatetech/common/item/armor/CopperArmorItem.java
@@ -0,0 +1,20 @@
+package heckerpowered.ultimatetech.common.item.armor;
+
+import heckerpowered.ultimatetech.UltimateTech;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+
+public class CopperArmorItem extends ArmorItem {
+
+    public CopperArmorItem(ArmorMaterial p_40386_, EquipmentSlot p_40387_, Properties p_40388_) {
+        super(p_40386_, p_40387_, p_40388_);
+    }
+
+    @Override
+    public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
+        return UltimateTech.ARMOR_PATH + (slot == EquipmentSlot.LEGS ? "copper_layer_2.png" : "copper_layer_1.png");
+    }
+}
